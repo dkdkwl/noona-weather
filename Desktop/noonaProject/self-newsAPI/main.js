fb31f902fd9dd3e27742547a1944ba82357225cd
dkdkwl <dkdkwl@naver.com>
2022-06-07 13:41:53 +0900

    클릭시 메뉴 변경 수정

--- Desktop/noonaProject/self-newsAPI/main.js
+++ Desktop/noonaProject/self-newsAPI/main.js
@@ -1,51 +1,51 @@
     let news;
-    let menus = document.querySelectorAll("#menu-list > button");    
-    let url = "";
-    let newsHTML = '';
+    let url = ``;
 
-    let getLatestNews = async()=>{
-        url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=1`);
-        let header = new Headers({'x-api-key' : '9cOmEeBlDd_8GHfIb6748kBIaLAIVEafVSWqCndLz6o'})
-        let response = await fetch(url,{headers : header})
+    let renderSorce = async ()=>{
+        let hedaer = new Headers({'x-api-key':'9cOmEeBlDd_8GHfIb6748kBIaLAIVEafVSWqCndLz6o'});
+        let response = await fetch(url,{headers : hedaer});
         let data = await response.json();
-        news = data.articles;    
+        news = data.articles;
         render();
     }
-    
 
-    let getNewsByTopic = async(event)=>{
-        let currentMenu = event.currentTarget.textContent.toLowerCase();
-        url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${currentMenu}&page_size=1`)
-        let header = new Headers({'x-api-key' : '9cOmEeBlDd_8GHfIb6748kBIaLAIVEafVSWqCndLz6o'})
-        let response = await fetch(url,{headers : header})
-        let data = await response.json();
-        news = data.articles;    
-        render();
+    let getNewscatcher = async()=>{
+        url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10`);
+        renderSorce();
+        console.log(news);
+        
+    }
 
+    let getButtonMenu = async(event)=>{
+        let currentMenu = event.target.textContent.toLowerCase();        
+        url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${currentMenu}&page_size=10`);
+        renderSorce();
     }
 
     let render = ()=>{
-        newsHTML = '';
-        newsHTML = news.map((items) =>{
+        let newsHTML = '';
+        newsHTML = news.map((items)=>{
             return `<div class="news row">
-                <div class="col-lg-4">
-                    <img class="news-img"
-                        src="${items.media}" />
-                </div>
-                <div class="col-lg-8">
-                    <h1>${items.title}</h1>
-                    <p>${items.summary}</p>
-                    <div>${items.rights} * ${items.published_date}</div>
-                </div>
-            </div>`
+                        <div class="col-lg-4">
+                            <img class="news-img" src="${items.media}" />
+                        </div>
+                        <div class="col-lg-8">
+                            <h1>${items.title}</h1>
+                            <p>${items.summary}</p>
+                            <div>${items.rights} * ${items.published_date}</div>
+                        </div>
+                    </div>`
         }).join('');
-        document.getElementById('news-board').innerHTML = newsHTML;
+        //console.log(newsHTML)
+        document.getElementById("news-board").innerHTML = newsHTML;
     }
 
-    menus.forEach((menu) => {
-        menu.addEventListener("click",(event)=>{
-            getNewsByTopic(event);
+    let menuList = document.querySelectorAll('.menus > button');
+    menuList.forEach(menu => {menu.addEventListener("click",(event)=>{
+            getButtonMenu(event);
         });
     });
 
-    getLatestNews();
+
+
+    getNewscatcher();
