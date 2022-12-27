a71a5074e33180832108876261006bb14d701c1c
dkdkwl <dkdkwl@naver.com>
2022-12-27 17:23:55 +0900

    부트스트랩 설치

---
+++ Desktop/noona/hnm-react-router-practice/src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src="https://lp2.hm.com/hmgoepprod?set=source[/c3/9a/c39a0a72e854d4e3fa762dafca5b1d82b3ec960b.jpg],origin[dam],category[ladies_premium_selection_shoes],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&amp;call=url[file:/product/main]" alt="" />
+        <div>Conscious choice</div>
+        <div>벨티드 트윌 코트</div>
+        <div>$60000</div>
+        <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- Desktop/noona/hnm-react-router-practice/src/page/ProductAll.js
+++ Desktop/noona/hnm-react-router-practice/src/page/ProductAll.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+    const [productList,setProductList] = useState([]);
+    const getProducts = async ()=>{
+        let url = `http://localhost:3004/products`;
+        let response = await fetch(url);
+        let data = await response.json();
+        setProductList(data);
+    }
+    useEffect(()=>{
+        getProducts();
+    }
+    ,[])
   return (
-    <div>전체 상품 페이지</div>
+    <div>
+        <ProductCard/>
+    </div>
   )
 }
 
